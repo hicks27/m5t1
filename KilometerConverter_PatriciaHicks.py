c08f402007d62bc518ae09e5045cc7e45ccdc025
hicks27 <hicksp2597@student.faytechcc.edu>
2017-06-30 19:26:12 -0700

    Add files via upload
    m4t1 completed

---
+++ KilometerConverter_PatriciaHicks.py
@@ -0,0 +1,5 @@
+Python 3.6.1 (v3.6.1:69c0db5, Mar 21 2017, 17:54:52) [MSC v.1900 32 bit (Intel)] on win32
+Type "copyright", "credits" or "license()" for more information.
+>>> 
+============ RESTART: G:/M5T1_KilometerConverter_PatriciaHicks.py ============
+Please enter the distance in kilometers: 
